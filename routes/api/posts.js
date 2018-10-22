eed75468bb8a5b51fa91644c3415114c6f4289b4
ellamarie005 <ellatolentino05@gmail.com>
2018-10-22 09:58:49 -0700

    added comment addition and deletion to a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -124,4 +124,51 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
     });
 });
 
+// route: this is a POST request api/posts/comment/:id
+// description: Add comment to a post
+// access: Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+  // check validate
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  Post.findById(req.params.id)
+    .then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.body.id,
+      };
+
+      // Add to comments array
+      post.comments.unshift(newComment);
+      //Save
+      post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+// route: this is a delete request api/posts/comment/:id/:comment_id
+// description: delete comment to a post
+// access: Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      // Check if comment exists
+      if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({commentnotexist: 'Comment does not exist'})
+      }
+      // Get remove index
+      const removeIndex = post.comments.map(item => item._id.toString())
+      .indexOf(req.params.comment_id);
+
+      // Splice comment out of the array
+      post.comments.splice(removeIndex, 1);
+      post.save().then(post => res.json(post));
+
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
 module.exports = router;
