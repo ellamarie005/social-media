019fc8219bdf16dcd52ab95ef07605df28533e6f
ellamarie005 <ellatolentino05@gmail.com>
2018-10-28 22:56:28 -0700

    connected redux devtools to google chrome

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducer';
+import rootReducer from './reducers';
 
+const initialState = {};
 const middleware = [thunk];
-const store = createStore(() => [], {}, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    // the next line is to allow redux devtools on google chrome to work -- to see state and the changes
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 export default store;
