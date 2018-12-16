9513cbaf20db8f812697f25a3bff0face2e65478
ellamarie005 <ellatolentino05@gmail.com>
2018-12-15 16:50:23 -0800

    creating profile actions and reducer

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,13 @@
+import { GET_PROFILE, PROFILE_LOADING } from '../actions/types'
+const initialState = {
+  profile: null,
+  profiles: null,
+  loading: false
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
