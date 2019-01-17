dee9815957ae712d33b64a3a8c177276347d8fa8
ellamarie005 <ellatolentino05@gmail.com>
2019-01-16 21:17:25 -0800

    started with dashboard buttons

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -20,7 +21,12 @@ class Dashboard extends Component {
     } else {
       // Check if logged in user has a profile data
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO:DISPLAY PROFILE</h4>
+        dashboardContent = (
+          <div>
+            <p className='lead text-muted'>Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link></p>
+            <ProfileActions/>
+          </div>
+        )
       } else {
         // User is logged in but has no profile
         dashboardContent = (
