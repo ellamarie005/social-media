d01a0d4928d0a76db088b8d4858d9c7d8edab7c7
ellamarie005 <ellatolentino05@gmail.com>
2019-01-16 21:52:51 -0800

    created edit profile page and will now update all changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,8 +19,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">Dashboard</Link>
+        </li>
+        <li className="nav-item">
           <a
-          // have to add spaces between quotes on href
+            // have to add spaces between quotes on href
             href=" "
             onClick={this.onLogoutClick.bind(this)}
             className="nav-link"
