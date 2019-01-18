116a8b208df332fff54d5d17540cb5499ef74cc2
ellamarie005 <ellatolentino05@gmail.com>
2019-01-17 18:37:34 -0800

    Input groups been placed in Add Experience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -33,6 +33,57 @@ class AddExperience extends Component {
               <h1 className='display -4 text-center'>Add Experience</h1>
               <p className='lead text-center'> Add any job or position that you have had in the past or current.</p>
               <small className='d-block pb-3'>* = required field</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder='* Company'
+                  name='company'
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  errors={errors.company}
+                />
+                <TextFieldGroup
+                  placeholder='* Job Title'
+                  name='title'
+                  value={this.state.title}
+                  onChange={this.onChange}
+                  errors={errors.title}
+                />
+                <TextFieldGroup
+                  placeholder='Location'
+                  name='location'
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  errors={errors.location}
+                />
+                <h6>From Date</h6>
+                <TextFieldGroup
+                  name='from'
+                  type='date'
+                  value={this.state.from}
+                  onChange={this.onChange}
+                  errors={errors.from}
+                />
+                <h6>To Date</h6>
+                <TextFieldGroup
+                  name='to'
+                  type='date'
+                  value={this.state.to}
+                  onChange={this.onChange}
+                  errors={errors.to}
+                  disabled={this.state.disabled ? 'disabled' : ''}
+                />
+                <div className='form-check mb-4'>
+                  <input type='checkbox'
+                  className='form-check-input'
+                  name='current'
+                  value={this.state.current}
+                  checked={this.state.current}
+                  onChange={this.onCheck}
+                  id='current'
+                  />
+                  <label htmlFor='current' className='form-check-label'>Current Job</label>
+                </div>
+              </form>
             </div>
           </div>
         </div>
