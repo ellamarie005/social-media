9dd04c7787ed7e7b59c80c90a9ff605c54b848dd
ellamarie005 <ellatolentino05@gmail.com>
2019-01-22 18:35:52 -0800

    started to create searchable profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../common/Spinner';
-import {getProfiles} from '../../actions/profileActions';
+import { getProfiles } from '../../actions/profileActions';
 
 class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
   render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        <h1>PROFILES HERE</h1>
+      } else {
+        profileItems = <h4>No Profiles found...</h4>
+      }
+    }
     return (
-      <div>
-        
+      <div className='profiles'>
+        <div className='container'>
+          <div className='row'>
+          <div className='col-md-12'>
+          <h1 className='display-4 text-center'>Developer Profiles</h1>
+          <p className='lead text-center'>Browse and connect with developers</p>
+          {profileItems}
+          </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -23,4 +46,4 @@ const mapStateToProps = state => ({
   profile: state.profile
 })
 
-export default connect(mapStateToProps, {getProfiles})(Profiles);
+export default connect(mapStateToProps, { getProfiles })(Profiles);
