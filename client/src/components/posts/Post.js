3020d7a65fbaed4582da128adc6623fbb168b0e4
ellamarie005 <ellatolentino05@gmail.com>
2019-02-26 17:07:07 -0800

    creating post page

---
+++ client/src/components/posts/Post.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import PostForm from './PostForm';
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Post;
