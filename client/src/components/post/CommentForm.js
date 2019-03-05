f5d1034031dd3c1762636c1711cdcd8ac9814739
ellamarie005 <ellatolentino05@gmail.com>
2019-03-04 21:58:40 -0800

    created delete button for comments on posts

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addComment } from '../../actions/postActions';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { addComment } from '../../actions/postActions';
 
 class CommentForm extends Component {
   constructor(props) {
@@ -10,25 +10,24 @@ class CommentForm extends Component {
     this.state = {
       text: '',
       errors: {}
-    }
+    };
+
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
   componentWillReceiveProps(newProps) {
     if (newProps.errors) {
-      this.setState({errors: newProps.errors})
+      this.setState({ errors: newProps.errors });
     }
   }
 
   onSubmit(e) {
     e.preventDefault();
+
     const { user } = this.props.auth;
     const { postId } = this.props;
+
     const newComment = {
       text: this.state.text,
       name: user.name,
@@ -36,46 +35,55 @@ class CommentForm extends Component {
     };
 
     this.props.addComment(postId, newComment);
-    this.setState({ text: '' })
+    this.setState({ text: '' });
+    console.log(user);
   }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     const { errors } = this.state;
+    
     return (
       <div className="post-form mb-3">
         <div className="card card-info">
           <div className="card-header bg-info text-white">
-            Make a Comment.
-        </div>
+            Make a comment...
+          </div>
           <div className="card-body">
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <TextAreaFieldGroup
-                  placeholder='Reply to post.'
-                  name='text'
+                  placeholder="Reply to post"
+                  name="text"
                   value={this.state.text}
                   onChange={this.onChange}
                   error={errors.text}
                 />
               </div>
-              <button type="submit" className="btn btn-dark">Submit</button>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 CommentForm.propTypes = {
   addComment: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired,
-  postId: PropTypes.string.isRequired
-}
+  postId: PropTypes.string.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-  errors: state.errors,
-  auth: state.auth
-})
+  auth: state.auth,
+  errors: state.errors
+});
 
 export default connect(mapStateToProps, { addComment })(CommentForm);
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/postActions';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
 
 class Post extends Component {
   componentDidMount() {
@@ -21,9 +22,11 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
-          <CommentForm postId={post._id}/>
+          <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       )
+      console.log(post.comments)
     }
     return (
       <div className='post'>
