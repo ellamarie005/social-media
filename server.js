027e5b39ec1163af23b2a317d1fb12cbc83528f2
ellamarie005 <ellatolentino05@gmail.com>
2019-03-05 16:35:08 -0800

    heroku deploy

--- server.js
+++ server.js
@@ -49,4 +49,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${port}`));
