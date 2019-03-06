6463c8d73335b1faa22218a0a393b6cc015d84a2
ellamarie005 <ellatolentino05@gmail.com>
2019-03-05 16:43:08 -0800

    fixed hosting errors

--- server.js
+++ server.js
@@ -48,5 +48,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000;
+const host = '0.0.0.0';
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${port}`));
+app.listen(port, host, () => console.log(`Server running on port ${port}`));
